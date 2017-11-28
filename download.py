17c597f5cce4b03f1756b5b1a6dca6f504dc637d
Rouge <code@r0uge.org>
2017-11-27 22:07:10 -0800

    Document code

--- download.py
+++ download.py
@@ -15,11 +15,13 @@ def download(url):
 
 def _images_get_next_item(s):
     start_line = s.find('rg_di')
-    if start_line == -1:    #If no links are found then give an error!
+    if start_line == -1:
+        #If no links are found then give an error!
         end_quote = 0
         link = "no_links"
         return link, end_quote
     else:
+        #Search through the html for images and take the links
         start_line = s.find('"class="rg_meta"')
         start_content = s.find('"ou"', start_line+1)
         end_content = s.find(',"ow"', start_content+1)
@@ -33,36 +35,36 @@ def _images_get_all_items(page):
         if next_img[0] == "no_links":
             break
         else:
-            items.append(next_img[0])      #Append all the links in the list named 'Links'
+            items.append(next_img[0])
+            #Add the link we just found and chop the html so the same link doesn't come up twice
             page = page[next_img[1]:]
     return items
 
-#Download Image Links
 def start(queries):
+    """Do the thing"""
     for keyword in queries:
         print("Searching for " + keyword)
         items = []
         search = keyword.replace(' ', '%20')
-        #make a search keyword  directory
+        #make the keyword url sanitized
         try:
             os.makedirs(keyword, exist_ok=True)
         except OSError as e:
             raise
         url = 'https://www.google.com/search?q=' + search + '&espv=2&biw=1366&bih=667&site=webhp&source=lnms&tbm=isch&sa=X&ei=XosDVaCXD8TasATItgE&ved=0CAcQ_AUoAg'
+        #Get the html of the google images page then search it for links
         raw_html = download(url)
         items = _images_get_all_items(raw_html)
-        #print ("Image Links = "+str(items))
         print("Total Image Links: " + str(len(items)))
-
         print("Starting Download...")
-
-        ## To save imges to the same directory
-        # IN this saving process we are just skipping the URL if there is any error
+        #Save imgaes to their directories
+        #Also skip the image if anythin is wrong 
         i = 0
         for link in items:
             try:
                 req = urllib.request.Request(link, headers={"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
                 res = urllib.request.urlopen(req, None, 15)
+                #Create a jpg dile and write the image binary data to it
                 with open(keyword + "/" + str(i) + ".jpg", 'wb') as f:
                     data = res.read()
                     f.write(data)
--- image-avg-color.py
+++ image-avg-color.py
@@ -1,9 +1,11 @@
+"""Searches google for images then sorts them by color"""
 import os
 from shutil import copyfile
 import math
 from colorthief import ColorThief
 import download
 
+#Set up the colors we want to check
 COLORS = {
     'red': (255, 0, 0),
     'orange': (255, 128, 0),
@@ -16,27 +18,37 @@ COLORS = {
     'purple': (128, 0, 255),
     'pink': (255, 0, 128),
     'brown': (173, 115, 0),
-    'black': (0, 0, 0)
+    'black': (0, 0, 0),
+    'white': (255,255,255)
 }
 
 def main(queries):
+    """Do the thing"""
     print("Getting images")
-    images = {}
+    #First we download the images
     download.start(queries)
+    images = {}
     print("Sorting images")
+    #Go through the folders of everything we searched for
     for dire in queries:
+        #Find all the images in the folder
         files = os.listdir(dire)
         for file in files:
             path = dire + "/" + file
             print("Crunching " + path)
+            #Skip images that are too large or small
             if os.stat(path).st_size > 7E6:
                 print("Skipping big file")
                 continue
             elif os.stat(path).st_size < 6.6E4:
                 print("Skipping small file")
                 continue
+            #Get color
             color_t = ColorThief(path)
             img_color = color_t.get_color(quality=10)
+            #Try to match the color we got with a color we know
+            #Because we use rgb we can use pythagoran theorem to find the distance and minimize it
+            #TODO: use weighted algorithim
             color_match = ""
             dist = 1E3
             for color, value in COLORS.items():
@@ -48,6 +60,7 @@ def main(queries):
                     dist = t_del
                     color_match = color
             print("Got " + color_match)
+            #Add the image to the stack of images in the matched color
             if color_match in images:
                 images[color_match].append(path)
             else:
@@ -55,6 +68,7 @@ def main(queries):
 
     print("Done sorting \nCopying...")
     os.makedirs("colors", exist_ok=True)
+    #Go through all the colors we did find and copy the images
     for cat, paths in images.items():
         os.makedirs("colors/" + cat, exist_ok=True)
         i = 0
@@ -64,6 +78,7 @@ def main(queries):
     print("Done!")
 
 if __name__ == "__main__":
+    #If this is the main instance run, otherwise you have to call main
     print("Queries:")
     QUE = input().split(",")
     main(QUE)
